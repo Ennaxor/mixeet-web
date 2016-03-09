28b99b4c40c083dc6ed58a42f3d473ccb764f0a5
Roxanne Lopez van Dooren <roxanne.lopez.dooren@gmail.com>
2016-03-09 12:13:21 +0100

    Changing routes

--- public/app/app.js
+++ public/app/app.js
@@ -12,7 +12,83 @@ mixeet.config(function($stateProvider, $urlRouterProvider, $locationProvider){
 	$urlRouterProvider.otherwise("/");
 	
 	$stateProvider
-		.state('home', {   
+		.state('/landing', {
+			url: "/landing",
+			onEnter: function ($rootScope) {
+	            if (getJSONLocal("auth")) {
+	                $rootScope.go("app.home");
+	            }
+        	},
+        	templateUrl: "index.html",
+			controller: "landingCtrl"
+		})
+		.state('app', {
+			url: "/",
+			onEnter: function ($rootScope) {
+	            if (!getJSONLocal("auth")) {
+	                $rootScope.go("landing");
+	            }
+	        },
+			templateUrl: "index.html",
+			controller: "headerCtrl"
+		})
+		.state('app.home', {
+			url: "home",
+			onEnter: function ($rootScope) {
+	            if (!getJSONLocal("auth")) {
+	                $rootScope.go("landing");
+	            }
+	        },
+			views: {
+	            content: {
+	                templateUrl: '/home/main.tpl.html',
+	                controller: 'homeCtrl'
+	            	}
+	        	}  
+		})
+		.state('app.collections', {
+			url: "collections",
+			onEnter: function ($rootScope) {
+	            if (!getJSONLocal("auth")) {
+	                $rootScope.go("landing");
+	            }
+	        },
+			views: {
+	            content: {
+	                templateUrl: '/collections/main.tpl.html',
+	                controller: 'collectionsCtrl'
+	            	}
+	        	}  
+		})
+		.state('app.events', {
+			url: "events",
+			onEnter: function ($rootScope) {
+	            if (!getJSONLocal("auth")) {
+	                $rootScope.go("landing");
+	            }
+	        },
+			views: {
+	            content: {
+	                templateUrl: '/events/main.tpl.html',
+	                controller: 'eventsCtrl'
+	            	}
+	        	}  
+		})
+		.state('app.achievements', {
+			url: "achievements",
+			onEnter: function ($rootScope) {
+	            if (!getJSONLocal("auth")) {
+	                $rootScope.go("landing");
+	            }
+	        },
+			views: {
+	            content: {
+	                templateUrl: '/achievements/main.tpl.html',
+	                controller: 'achievementsCtrl'
+	            	}
+	        	}  
+		})
+	/*	.state('home', {   
 	      url: "/",
 	      templateUrl: static_path+"/home/main.tpl.html",
 	      controller: 'homeCtrl'
@@ -36,7 +112,7 @@ mixeet.config(function($stateProvider, $urlRouterProvider, $locationProvider){
 			url: "/achievements",
 	      	templateUrl: static_path+"/achievements/main.tpl.html",
 	      	controller: 'achievementsCtrl'
-		});
+		});*/
 
 
 
