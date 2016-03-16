34759f8d35bc435f7bba9936482e77824346054d
Roxanne Lopez van Dooren <roxanne.lopez.dooren@gmail.com>
2016-03-16 13:14:34 +0100

    prod url

--- public/app/services/apiSvc.js
+++ public/app/services/apiSvc.js
@@ -1,16 +1,20 @@
 webAppFactory.apiSvc = function($resource){
 	return {
-        env:"dev",
+        env:"prod",
+        prod:{
+            protocol:"https",
+            host:"mixeet.herokuapp.com",
+            port:""
+        },
         dev:{
             protocol:"http",
             host:"localhost",
-            port:"5000",
-            root:"api"
+            port:"5000"
         },
 
         getApiPath:function(path){
             var api=this[this.env];
-            return api.protocol+"://"+api.host+(api.port!==""?(":"+api.port):"")+"/"+api.root+"/"+path;
+            return api.protocol+"://"+api.host+(api.port!==""?(":"+api.port):"")+"/"+path;
         },
 
         rest:function(path, methods, defaultParams){
